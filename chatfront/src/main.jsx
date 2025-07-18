9f90d42f5e063f167f96a60e25581a283748ed4c
vishal <141538917+vkprajapati96@users.noreply.github.com>
2025-07-18 12:50:33 +0530

    Update main.jsx

--- chatfront/src/main.jsx
+++ chatfront/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 
-export  const serverUrl ="http://localhost:8000"
+export  const serverUrl ="https://realtime-chatapp-backend-leod.onrender.com"
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
