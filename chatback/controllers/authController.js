f4ef87ae3e3c429f781f6320b179f9de2b35da77
vishal <141538917+vkprajapati96@users.noreply.github.com>
2025-07-18 13:09:12 +0530

    Update authController.js

--- chatback/controllers/authController.js
+++ chatback/controllers/authController.js
@@ -37,8 +37,8 @@ export const signUp = async (req, res) => {
         res.cookie("token", token, {
           httpOnly: true,
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-          sameSite: "strict",
-          secure: false,
+          sameSite: "None",
+          secure: true,
         });
     
 
@@ -70,8 +70,8 @@ export const login = async (req, res) => {
   res.cookie("token", token, {
           httpOnly: true,
           maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-          sameSite: "strict",
-          secure: false,
+          sameSite: "None",
+          secure: true,
         });
            
 
